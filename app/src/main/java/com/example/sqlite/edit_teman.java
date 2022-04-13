2160cb54a6c389e894ec0ca2f7b6116f8b1692e7
Aalfinuraziz <alfinuraziz885@gmail.com>
2022-04-14 06:18:31 +0700

    Activity 7

---
+++ app/src/main/java/com/example/sqlite/edit_teman.java
@@ -0,0 +1,14 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_teman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_teman);
+    }
+}
